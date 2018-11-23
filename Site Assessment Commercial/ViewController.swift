f5795a8cad46ad98bd370e285cd8b6d3d6cbd8d3
PSC-IT-Admin <it@polaronsolar.com>
2018-11-23 11:08:27 -0500

    Initial Commit

---
+++ Site Assessment Commercial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Site Assessment Commercial
+//
+//  Created by ChenYu on 2018-11-23.
+//  Copyright © 2018 chyapp.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
